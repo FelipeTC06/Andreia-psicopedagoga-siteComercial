f18d9300d5911459593ece4b26fc10284a5a4ba8
Felipe <felipe@easyteam.rocks>
2023-06-21 22:51:32 -0300

    create new components

--- src/app/components/dashboard-module/dashboard.module.ts
+++ src/app/components/dashboard-module/dashboard.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
